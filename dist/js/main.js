956c00f958682f2c726c9f5c3a29aaba10e839c2
Dante Allievi <toto99.-@hotmail.com>
2021-05-08 12:32:19 +0200

    First commit

---
+++ dist/js/main.js
@@ -0,0 +1,35 @@
+// DOM ITEMS
+const menuBtn = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItem = document.querySelectorAll(".nav-item");
+const header = document.querySelector(".container");
+
+// SET INITAL STATE OF MENU
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItem.forEach((item) => item.classList.add("show"));
+
+    // SET MENU STATE
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItem.forEach((item) => item.classList.remove("show"));
+
+    // SET MENU STATE
+    showMenu = false;
+  }
+}
+
